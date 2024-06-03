1c4c54a7e2a56192724e25f233b5380fc25480eb
Aime Brues <72033556+SHYAKA-Aime@users.noreply.github.com>
2024-06-03 14:13:23 +0200

    Create tokenGenerator.utils.ts

---
+++ tokenGenerator.utils.ts
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+import { UserAttributes } from "../types/user.types";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const generateToken = async (user: UserAttributes) => {
+  return jwt.sign(
+    {
+      role: user.role,
+      email: user.email,
+      id: user.id,
+    },
+    process.env.JWT_SECRET || "",
+    {
+      expiresIn: "24h",
+    }
+  );
+};
+export { generateToken };
+
+
+
+export const generateResetToken = (user: UserAttributes) => {
+  return jwt.sign({ id: user.id }, process.env.JWT_SECRET as string, {
+    expiresIn: "1h", // Token expires in 1 hour
+  });
+};
+
+export const decodeToken = (token: string): any => {
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET as string);
+  } catch (error) {
+    throw new Error("Invalid or expired token");
+  }
+};
