1d4613d844c9bd406725fa4a7f3bc346086ba101
Aime Brues <72033556+SHYAKA-Aime@users.noreply.github.com>
2024-06-03 14:12:37 +0200

    Create user.route.ts

---
+++ user.route.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import {
+  updateRole,
+  userSignup,
+  userLogin,
+  changeAccountStatus,
+  updatePassword,
+  requestPasswordReset,
+  resetPassword
+} from "../controllers/user.controller";
+import { protectRoute, restrictTo } from "../middlewares/auth.middleware";
+import { validateUser, validateUserLogin,validateUserUpdatePassword} from "../validations/user.validate";
+import { userRole } from "../utils/variable.utils";
+
+const userRoutes = express.Router();
+
+userRoutes.post("/signup", validateUser, userSignup);
+userRoutes.patch("/:id", protectRoute, restrictTo(userRole.admin), updateRole);
+userRoutes.patch(
+  "/change-account-status/:id",
+  protectRoute,
+  restrictTo(userRole.admin),
+  changeAccountStatus
+);
+
+userRoutes.post('/login', validateUserLogin, userLogin);
+userRoutes.patch('/:id/updatepassword',protectRoute,validateUserUpdatePassword, updatePassword);
+userRoutes.post("/requestpasswordreset", requestPasswordReset);
+userRoutes.post("/resetpassword", resetPassword);
+export default userRoutes;
